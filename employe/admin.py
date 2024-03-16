cdfc5527212df55801c34e0f3f737815851e673a
Bitsapascal <bitapascal033@gmail.com>
2024-03-16 23:31:57 +0100

    utilisation de l'interface d'administration de django: Django Admin

--- employe/admin.py
+++ employe/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from employe.models import Personne
+from employe.models import Employe
 
 # Register your models here.
+admin.site.register(Personne)
+admin.site.register(Employe)
+
